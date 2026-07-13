3c987b7889706068f5bf42ad1c3fece623e30d6b
Ronak_chy_33 <ronak.chaudhary25@spit.ac.in>
2026-07-13 17:11:46 +0530

    variable rules with examples and corrections
    Added examples of invalid variable names and corrected formatting.

---
+++ python/varibalerules.py
@@ -0,0 +1,14 @@
+a = 23
+
+aaa = 435
+
+ronakkkk = 34
+ronak1 =34
+
+ronakk = 45
+
+_ronakk = 34
+
+# @sameer = 56 # Invalid due to @ symbol
+# s@meer # Invalid due to @ symbol
+# 1ronak = 45 it's an invalid intialization
