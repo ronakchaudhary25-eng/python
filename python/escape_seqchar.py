afa58fda83c9eb38f53c0da277f0d5e7249591dc
Ronak_chy_33 <ronak.chaudhary25@spit.ac.in>
2026-07-14 12:24:10 +0530

    Add escape sequences demonstration script
    This script demonstrates various escape sequences in Python and their outputs.

---
+++ python/escape_seqchar.py
@@ -0,0 +1,70 @@
+# \n -> New Line
+print("Hello\nWorld")
+# Output:
+# Hello
+# World
+
+
+# \t -> Horizontal Tab
+print("Name\tAge")
+# Output:
+# Name    Age
+
+
+# \\ -> Backslash
+print("C:\\Users\\Ronak\\Desktop")
+# Output:
+# C:\Users\Ronak\Desktop
+
+
+# \' -> Single Quote
+print('It\'s Python')
+# Output:
+# It's Python
+
+
+# \" -> Double Quote
+print("He said \"Hello!\"")
+# Output:
+# He said "Hello!"
+
+
+# \b -> Backspace
+print("ABC\bD")
+# Output:
+# ABD
+
+
+# \r -> Carriage Return
+print("Hello\rBye")
+# Output:
+# Byelo
+
+
+# \f -> Form Feed
+print("Hello\fWorld")
+# Output:
+# Hello World
+# (Terminal dependent)
+
+
+# \v -> Vertical Tab
+print("Hello\vWorld")
+# Output:
+# Hello
+#      World
+# (Terminal dependent)
+
+
+# \a -> Alert / Bell
+print("Beep!\a")
+# Output:
+# Beep!
+# (May produce a beep sound)
+
+
+# \0 -> Null Character
+print("ABC\0DEF")
+# Output:
+# ABCDEF
+# (Null character is invisible)
